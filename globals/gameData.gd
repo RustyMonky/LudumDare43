212cfad7db2fdbfbf578109f487c544850b83ec6
Ian <ian.cantor@fandm.edu>
2018-12-02 10:01:58 -0500

    Implements carousel option selection, adds gameData global

---
+++ globals/gameData.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var chosenDiety = null
+
+func _ready():
+	pass
--- ui/carousel/carousel.gd
+++ ui/carousel/carousel.gd
@@ -1,6 +1,8 @@
 extends HBoxContainer
 
+onready var chooseButton = $chooseButton
 onready var optionLabel = $optionLabel
+onready var tween = $tween
 
 var currentOption = 0
 var options = []
@@ -17,6 +19,7 @@ func setOptions(optionsArray):
 
 # Signals
 
+# Moves carousel option to left
 func _on_arrowLeft_pressed():
 	if currentOption - 1 < 0:
 		currentOption = options.size() - 1
@@ -25,6 +28,7 @@ func _on_arrowLeft_pressed():
 
 	optionLabel.text = options[currentOption]
 
+# Moves carousel option to right
 func _on_arrowRight_pressed():
 	if currentOption + 1 > options.size() - 1:
 		currentOption = 0
@@ -32,3 +36,17 @@ func _on_arrowRight_pressed():
 		currentOption += 1
 
 	optionLabel.text = options[currentOption]
+
+# Assigns the diety to the player
+func _on_chooseButton_pressed():
+	chooseButton.disabled = true
+	gameData.chosenDiety = options[currentOption]
+
+# Fades out the carousel
+func _on_chooseButton_button_up():
+	tween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.start()
+
+# Removes the carousel after a completed fade
+func _on_tween_tween_completed(object, key):
+	self.queue_free()
