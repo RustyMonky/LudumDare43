9162dc31c6679ce979d8dd527e1a881cbd796812
Ian <ian.cantor@fandm.edu>
2018-12-02 11:27:51 -0500

    Adds new theme for deity panel h separation, loads followers upon deity selection

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,8 +3,11 @@ extends Node2D
 onready var carousel = $ui/uiContainer/carousel
 onready var computerDeityPanel = $ui/uiContainer/deityHbox/computerDeityPanel
 onready var deityHbox = $ui/uiContainer/deityHbox
+onready var followerHbox = $ui/uiContainer/followerHbox
 onready var playerDeityPanel = $ui/uiContainer/deityHbox/playerDeityPanel
 onready var tween = $tween
+
+var followerIcon = load("res://assets/sprites/ui/icons/follower.png")
 
 func _ready():
 	carousel.setHeader("your deity")
@@ -18,3 +21,8 @@ func _on_carousel_tree_exited():
 	computerDeityPanel.setCount(gameData.computerFollowerCount)
 	tween.interpolate_property(deityHbox, "modulate", Color(1,1,1,0), Color(1,1,1,1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.start()
+
+	for i in range(gameData.playerFollowerCount):
+		var follower = TextureRect.new()
+		follower.texture = followerIcon
+		followerHbox.add_child(follower)
